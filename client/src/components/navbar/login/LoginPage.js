81cc2a1cf5d7bdcfe536f98d527e2de7be565f3e
Terry Wong <terry.wong@visier.com>
2021-07-30 17:06:39 -0700

    Added signup react component

--- client/src/components/navbar/login/LoginPage.js
+++ client/src/components/navbar/login/LoginPage.js
@@ -10,7 +10,6 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
@@ -97,7 +96,7 @@ function LoginPage() {
                                     </Link>
                                 </Grid>
                                 <Grid item>
-                                    <Link href="#" variant="body2">
+                                    <Link href="/register" variant="body2">
                                         {"Don't have an account? Sign Up"}
                                     </Link>
                                 </Grid>
