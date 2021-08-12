e33a56c8442755c560054e33dd4d5513af7c9bb3
Terry Wong <terry.wong@visier.com>
2021-08-12 10:36:23 -0700

    Fixed baseurl

--- client/src/actions/Authenticate.js
+++ client/src/actions/Authenticate.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const BASE_URL = process.env.BASE_URL;
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://rec-center-booking.herokuapp.com";
 
 export function loginUser(data) {
     axios.post(`${BASE_URL}/api/user/login`, data, {withCredentials: true, credentials: 'include'}).then( (response) => {
