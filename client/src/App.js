ff1882679a447d3449f21bbc23d58a500034d9fd
Jamie <b2c3b@ugrad.cs.ubc.ca>
2021-08-12 04:00:33 -0700

    Project Progress 6: Debugging deployment of calendars (x13)

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
            <Route path='/' exact component={Home}/>
            <Route exact path='/rec-center' >
                <RecCenterCalendar />
-           <Route />
+           </Route>
            <Route exact path='/user-cal' component={UserCalendar} />
            <Route exact path='/user/login' component={UserLoginPage}/>
            <Route exact path='/user/register' component={RegisterPage}/>
